3ea3af34b6123de38904e54e176ac7622ed82a17
gabriel <gdubatti@posthac.com.ar>
2015-06-02 11:46:40 -0300

    myproj.lexer added

---
+++ lexers/myproj.lua
@@ -0,0 +1,46 @@
+-- myproj LPeg lexer.
+
+local l = require('lexer')
+local token, word_match = l.token, l.word_match
+local P, R, S = lpeg.P, lpeg.R, lpeg.S
+
+local M = {_NAME = 'myproj'}
+
+-- Whitespace.
+local ws = token(l.WHITESPACE, l.space^1)
+
+local hidden = token('hidden', '::' * l.nonnewline^0)
+local style_hidden = 'notvisible'
+
+local group1 = token('group1', '[' * (l.nonnewline - ']')^0 * ']' )
+local style_group1 = 'fore:#400000,bold'
+
+local group2 = token('group2', '(' * (l.nonnewline - ')')^0 * ')' )
+local style_group2 = 'fore:#000040,bold'
+
+local group3 = token('group3', '<' * (l.nonnewline - '>')^0 * '>' )
+local style_group3 = 'fore:#000080,bold'
+
+local extension = token('extension', '.' * l.word^0 )
+local style_extension = 'fore:#005050,italics'
+
+M._rules = {
+  {'whitespace', ws},
+  {'hidden',     hidden},
+  {'group1',      group1},
+  {'group2',      group2},
+  {'group3',      group3},
+  {'extension',   extension},
+}
+
+M._tokenstyles = {
+   hidden   = style_hidden,
+   group1   = style_group1,
+   group2   = style_group2,
+   group3   = style_group3,
+   extension= style_extension,
+}
+
+M._FOLDBYINDENTATION = true
+
+return M
