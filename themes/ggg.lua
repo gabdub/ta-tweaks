e26ec964ba0ce40a0204f36f5d7a26a75d790a60
gabriel <gdubatti@posthac.com.ar>
2016-11-17 11:04:35 -0300

    ggg theme update

--- themes/ggg.lua
+++ themes/ggg.lua
@@ -1,6 +1,7 @@
 local buffer = buffer
 local property, property_int = buffer.property, buffer.property_int
 
+--COLORS--
 --current line
 property['color.curr_line_back']  = 0xf5f9ff
 property['color.caret']           = 0x362b00
@@ -8,16 +9,19 @@ property['color.caret']           = 0x362b00
 property['color.text_fore']       = 0x101000
 property['color.text_back']       = 0xd6e7ff
 --selection
-property['color.selection_fore']  = 0xd6e7ff
-property['color.selection_back']  = 0x101000
+property['color.selection_fore']  = 0x905090
+property['color.selection_back']  = 0x88acdf
+property['color.hilight']         = 0xa8ccff
+property['color.placeholder']     = 0x88acdf
+property['color.find']            = 0xa8ccff
 --calltips
 property['color.calltips_fore']   = 0x0a70ff
 property['color.calltips_back']   = 0x362b00
 --linenum/markers
 property['color.linenum_fore']    = 0x0a70ff
 property['color.linenum_back']    = 0xb8dcff
-property['color.folding']         = 0x6D6D3E
-property['color.folding_sel']     = 0x0a70ff
+property['color.markers']         = 0x6D6D3E --folding
+property['color.markers_sel']     = 0x0a70ff
 property['color.bookmark']        = 0x015aee
 property['color.warning']         = 0x0089b5
 property['color.error']           = 0x2f32dc
@@ -45,8 +49,7 @@ property['color.operator']        = 0x101000
 property['color.red']             = 0x2f32dc
 property['color.green']           = 0x006030
 
--- Default font.
---property['font'], property['fontsize'] = 'Bitstream Vera Sans Mono', 11
+--Default font
 property['font'], property['fontsize'] = 'Ubuntu Mono', 13
 if WIN32 then
   property['font'], property['fontsize'] = 'DejaVu Sans Mono', 11
@@ -54,60 +57,77 @@ elseif OSX then
   property['font'], property['fontsize'] = 'Monaco', 12
 end
 
--- Token styles.
-property['style.nothing'] = ''
-property['style.whitespace'] = ''
-property['style.identifier'] = ''
+--Predefined styles
+property['style.default'] = 'font:%(font),size:%(fontsize),fore:%(color.text_fore),back:%(color.text_back)'
+property['style.linenumber'] = 'fore:%(color.linenum_fore),back:%(color.linenum_back)'
+--property['style.controlchar'] = ''
+property['style.indentguide'] = 'fore:%(color.indentguide)'
+property['style.calltip'] = 'fore:%(color.calltips_fore),back:%(color.color.calltips_back)'
+
+--Token styles
 property['style.class'] = 'fore:%(color.class)'
 property['style.comment'] = 'fore:%(color.comment)'
 property['style.constant'] = 'fore:%(color.constant)'
+property['style.embedded'] = 'fore:%(color.embedded),back:%(color.curr_line_back)'
 property['style.error'] = 'fore:%(color.error),italics'
 property['style.function'] = 'fore:%(color.function)'
+property['style.identifier'] = ''
 property['style.keyword'] = 'fore:%(color.keyword)'
 property['style.label'] = 'fore:%(color.label)'
 property['style.number'] = 'fore:%(color.number)'
 property['style.operator'] = 'fore:%(color.operator)'
+property['style.preprocessor'] = 'fore:%(color.preprocessor)'
 property['style.regex'] = 'fore:%(color.regex)'
 property['style.string'] = 'fore:%(color.string)'
-property['style.preprocessor'] = 'fore:%(color.preprocessor)'
 property['style.type'] = 'fore:%(color.type)'
 property['style.variable'] = 'fore:%(color.variable)'
-property['style.embedded'] = 'fore:%(color.embedded),back:%(color.curr_line_back)'
+property['style.whitespace'] = ''
 
--- Predefined styles.
-property['style.default'] = 'font:%(font),size:%(fontsize),fore:%(color.text_fore),back:%(color.text_back)'
-property['style.linenumber'] = 'fore:%(color.linenum_fore),back:%(color.linenum_back)'
-property['style.bracelight'] = 'fore:%(color.brace_ok),underlined'
-property['style.bracebad'] = 'fore:%(color.error)'
-property['style.controlchar'] = '%(style.nothing)'
-property['style.indentguide'] = 'fore:%(color.indentguide)'
-property['style.calltip'] = 'fore:%(color.calltips_fore),back:%(color.color.calltips_back)'
+--Multiple Selection and Virtual Space
+--buffer.additional_sel_alpha =
+--buffer.additional_sel_fore =
+--buffer.additional_sel_back =
+--buffer.additional_caret_fore =
 
--- Caret and Selection Styles.
+--Caret and Selection Styles
 buffer:set_sel_fore(true, property_int['color.selection_fore'])
 buffer:set_sel_back(true, property_int['color.selection_back'])
 --buffer.sel_alpha =
 buffer.caret_fore = property_int['color.caret']
 buffer.caret_line_back = property_int['color.curr_line_back']
 --buffer.caret_line_back_alpha =
-buffer.caret_width= 2
-buffer.tab_width= 4
-buffer.use_tabs = false
 
--- Fold Margin.
+--Fold Margin
 buffer:set_fold_margin_colour(true, property_int['color.text_back'])
 buffer:set_fold_margin_hi_colour(true, property_int['color.text_back'])
 
--- Markers.
-local MARK_BOOKMARK, t_run = textadept.bookmarks.MARK_BOOKMARK, textadept.run
+--Markers
+local MARK_BOOKMARK = textadept.bookmarks.MARK_BOOKMARK
+--buffer.marker_fore[MARK_BOOKMARK] = property_int['color.text_fore']
 buffer.marker_back[MARK_BOOKMARK] = property_int['color.bookmark']
-buffer.marker_back[t_run.MARK_WARNING] = property_int['color.warning']
-buffer.marker_back[t_run.MARK_ERROR] = property_int['color.error']
+--buffer.marker_fore[textadept.run.MARK_WARNING] = property_int['color.text_fore']
+buffer.marker_back[textadept.run.MARK_WARNING] = property_int['color.warning']
+--buffer.marker_fore[textadept.run.MARK_ERROR] = property_int['color.text_fore']
+buffer.marker_back[textadept.run.MARK_ERROR] = property_int['color.error']
 for i = 25, 31 do -- fold margin markers
   buffer.marker_fore[i] = property_int['color.text_back']
-  buffer.marker_back[i] = property_int['color.folding']
-  buffer.marker_back_selected[i] = property_int['color.folding_sel']
+  buffer.marker_back[i] = property_int['color.markers']
+  buffer.marker_back_selected[i] = property_int['color.markers_sel']
 end
 
--- Long Lines.
+--Indicators
+buffer.indic_fore[ui.find.INDIC_FIND] = property_int['color.find']
+buffer.indic_alpha[ui.find.INDIC_FIND] = 255
+local INDIC_BRACEMATCH = textadept.editing.INDIC_BRACEMATCH
+buffer.indic_fore[INDIC_BRACEMATCH] = property_int['color.brace_ok']
+local INDIC_HIGHLIGHT = textadept.editing.INDIC_HIGHLIGHT
+buffer.indic_fore[INDIC_HIGHLIGHT] = property_int['color.hilight']
+buffer.indic_alpha[INDIC_HIGHLIGHT] = 255
+local INDIC_PLACEHOLDER = textadept.snippets.INDIC_PLACEHOLDER
+buffer.indic_fore[INDIC_PLACEHOLDER] = property_int['color.placeholder']
+
+--Call tips
+--buffer.call_tip_fore_hlt = property_int['color.light_blue']
+
+--Long Lines
 buffer.edge_colour = property_int['color.curr_line_back']
