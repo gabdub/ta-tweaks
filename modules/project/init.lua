296ff7cdbd6e2d5566d2ab4ae69bc27e62f0224c
gabdub <gdubatti@gmail.com>
2021-02-22 09:20:10 -0300

    move VCS code to a new file

--- modules/project/init.lua
+++ modules/project/init.lua
@@ -91,6 +91,7 @@ Proj.PRJT_SEARCH= '[Project search]'  --search results
 
 require('project.proj_data')
 require('project.proj_ui')
+require('project.proj_vcs')
 require('project.proj_cmd')
 require('project.proj_ctags')
 require('project.proj_diff')
